19498928d975e60dcc045c6ba73f2faa617d0b70
Artur <artur-fedyuk@mail.ru>
2018-03-02 18:31:29 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
 module.exports = function solveSudoku(matrix) {
-  // your solution
-}
+    for (let row = 0; row < 9; row++) {
+        for (let col = 0; col < 9; col++) {
+            if (matrix[row][col] === 0) {
+                let candidates = getCandidates(row, col);
+                for (let candidate of candidates) {
+                    matrix[row][col] = candidate;
+                    let attempt = solveSudoku(matrix);
+                    if (attempt) return attempt;
+                }
+                matrix[row][col] = 0;
+                return false;
+            }
+        }
+    }
+    return matrix;
+
+    function getCandidates(row, col) {
+        let candidates = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+        let result = [];
+        let area = {
+            row: Math.floor(row / 3) * 3,
+            col: Math.floor(col / 3) * 3,
+        };
+
+        for (let i = 0; i < 9; i++) {
+            result.push([matrix[row][i], matrix[i][col], matrix[area.row + i % 3][area.col + Math.floor(i / 3)]])
+        }
+
+        candidates.filter((value) => !result.includes(value))
+        return result;
+    }
+};
